f34a2f309bebf713f04e86f6ea8acc34289f35c8
shengqishi <346001690@qq.com>
2018-08-26 21:30:48 +0800

    add nn_xor.py project codes

--- chapter10/pyimagesearch/nn/neuralnetwork.py
+++ chapter10/pyimagesearch/nn/neuralnetwork.py
@@ -63,7 +63,7 @@ class NeuralNetwork:
         # activation is a special case -- it's just the input
         # feature vector it self
         A = [np.atleast_2d(x)]
-
+        
         # FEEDFORWARD:
         # loop over the layers in the network
         for layer in np.arange(0, len(self.W)):
@@ -92,3 +92,63 @@ class NeuralNetwork:
         # simply the error of the output layer times the derivative
         # of our activation function for the output value
         D = [error * self.sigmoid_deriv(A[-1])]
+
+        # once you undersand the chain rule it becomes super easy
+        # to implement with a 'for' loop -- simply loop over the 
+        # layers in reverse order(ignoring the last two since we
+        # already have taken them into account)
+        for layer in np.arange(len(A) - 2, 0, -1):
+            # the delta for the current layer is equal to the delta
+            # of the *previous layer* dotted with the weight matrix
+            # of the current layer, followed by multiplying the delta
+            # by the derivative of the nonlinear activation function
+            # for the activations of the current layer
+            delta = D[-1].dot(self.W[layer].T)
+            delta = delta * self.sigmoid_deriv(A[layer])
+            D.append(delta)
+
+        # since we looped over our layers in reverse order we need to
+        # reverse the deltas
+        D = D[::-1]
+        
+        # WEIGHT UPDATE PHASE:
+        # loop over the layers
+        for layer in np.arange(0, len(self.W)):
+            # update our weights by taking the dot product of the layer
+            # activations with their respective deltas, then multiplying
+            # this value by some small learning rate and adding to our
+            # weight matrix -- this is where the actual "learning" takes
+            # place
+            self.W[layer] += -self.alpha * A[layer].T.dot(D[layer])
+
+    def predict(self, X, addBias=True):
+        # initialize the output prediction as the input features -- this
+        # value will be propagated through the network to obtain the 
+        # final prediction
+        p = np.atleast_2d(X)
+
+        # check to see if the bias column should be added
+        if addBias:
+            # insert a column 1's as the last entry in the feature matrix(bias)
+            p = np.c_[p, np.ones(p.shape[0])]
+
+        # loop over our layers in the network
+        for layer in np.arange(0, len(self.W)):
+            # computing the output prediction is as simple as taking
+            # the dot product between the current activation value 'p'
+            # and the weight matrix associated with the current layer,
+            # then passing this value through a nonlinear activation function
+            p = self.sigmoid(np.dot(p, self.W[layer]))
+
+        # return the predicted value
+        return p
+
+    def calculate_loss(self, X, targets):
+        # make predictions for the input data points then compute the loss
+        targets = np.atleast_2d(targets)
+        predictions = self.predict(X, addBias=False)
+        loss = 0.5 * np.sum((predictions - targets) ** 2)
+
+        # return the loss
+        return loss
+
